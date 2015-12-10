8748315120aeb44d1cc20b839bcd59ebafb12464
alex <sania_free@i.ua>
2015-12-10 18:37:45 +0200

    first comit

---
+++ js/common.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	//fancybox
+	$('.fancybox').fancybox({
+		helpers: {
+			overlay: {
+				locked: false
+			}
+		}
+	});
+	
+	//placeholder
+	$.support.placeholder = ('placeholder' in document.createElement('input'));
+
+	//bxslider
+	$('.bxslider').bxSlider({
+	  nextSelector: '#slider-next',
+	  prevSelector: '#slider-prev',
+	  nextText: 'Onward →',
+	  prevText: '← Go back'
+	});
+});
